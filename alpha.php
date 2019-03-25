9caacc88b16270702521cbb1e74188d5488099f7
siya9 <44136565+siya9@users.noreply.github.com>
2019-03-25 22:14:31 +0000

    Add files via upload

---
+++ alpha.php
@@ -0,0 +1,17 @@
+
+	<!DOCTYPE html>
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>alpha</title>
+		<meta name="therealsiya" content="this is my story"/>
+	</head>
+	<body>
+		<?php
+			$magic = ", when will it be my chance to show my magic.";
+			echo "hello again".$magic;
+		?>
+
+		<p><a href="index.html"> home</a></p>
+	</body>
+	</html>
---
+++ navigation.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<style type="text/css">
+
+		a.money
+		{
+			text-transform: capitalize; 
+			text-align: center;
+			color: ;
+		}
+
+		a.money:hover
+		{
+			color: #51cbf1;
+			text-decoration: underline;
+		}
+
+		a.money:active
+		{
+			color: yellow;
+			text-decoration: underline;
+		}
+
+	</style>
+</head>
+<body>
+<div class="head">
+<h1 style="font-family: 'times new roman'; text-transform: capitalize; text-align: center;">forex elite clan</h1>
+<header>
+		<nav>
+			<ul>
+				<li><a class="money" href="javascript: alert('money')" >home</a></li>
+				<li><a class="money" href="javascript: alert('money')">blog</a></li>
+				<li><a class="money" href="javascript: alert('money')">tools</a></li>
+				<li><a class="money" href="javascript: alert('money')">contacts</a></li>
+				<li><a class="money" href="javascript: alert('still making that money')">about</a></li>
+
+				
+			</ul>
+		</nav>
+	</header>
+</div>
+	<?php
+		$magic = ", when will it be my chance to show my magic.";
+		echo "hello again".$magic;
+	?>
+</body>
+</html>
